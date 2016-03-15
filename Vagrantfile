9993e4bbd3be246131503c6d88474e8dfa0eda6f
Christian Fei <crifei93@gmail.com>
2016-03-15 18:19:32 +0100

    provisioning discourse with rsync

--- Vagrantfile
+++ Vagrantfile
@@ -3,9 +3,12 @@ Vagrant.configure('2') do |config|
   config.vm.hostname = 'discourse.dev'
   config.ssh.shell = 'bash -c \'BASH_ENV=/etc/profile exec bash\''
   config.vm.network :private_network, ip: "192.168.11.3"
+  
   config.ssh.insert_key = false
+  config.ssh.forward_agent = true
   config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.synced_folder "./", "/vm", type: "nfs", :mount_options => ['nolock,vers=3,udp,noatime,actimeo=1']
+
   config.vm.provider "virtualbox" do |vb|
     vb.customize ["modifyvm", :id, "--memory", 2048]
     vb.customize ["modifyvm", :id, "--cpus", 2]
---
+++ discourse-source/image/build.rb
@@ -0,0 +1,54 @@
+# simple build file to be used locally by Sam
+#
+require 'pty'
+
+$version = "1.0.17"
+
+$docker_squash = "https://github.com/jwilder/docker-squash/releases/download/v0.2.0/docker-squash-linux-amd64-v0.2.0.tar.gz"
+
+$base_image = "discourse/base:#{$version}"
+$image = "discourse/discourse:#{$version}"
+$fast_switch= "discourse/discourse_fast_switch:#{$version}"
+$test = "discourse/discourse_test:#{$version}"
+
+if ENV["USER"] != "root"
+  STDERR.puts "Build script must be ran as root due to docker-squash"
+  exit 1
+end
+
+def run(command)
+  lines = []
+  PTY.spawn(command) do |stdin, stdout, pid|
+    begin
+      stdin.each do |line|
+        lines << line
+        puts line
+      end
+    rescue Errno::EIO
+      # we are done
+    end
+  end
+
+  lines
+end
+
+def ensure_docker_squash
+  return if File.exist?("docker-squash")
+  run ("wget #{$docker_squash}")
+  run ("tar -xzvf *.tar.gz")
+  run ("rm -f docker-squash-linux*")
+end
+
+ensure_docker_squash
+
+def build(path, tag, is_base)
+  lines = run("cd #{path} && docker build .")
+  img = lines[-1]["successfully built ".length..-1].strip
+
+  run("docker save #{img} | ./docker-squash -t #{tag} -verbose #{is_base && "-from root"} | docker load")
+end
+
+build("base",$base_image,true)
+build("discourse",$image,false)
+build("discourse_fast_switch",$fast_switch,false)
+build("discourse_test",$test,false)
