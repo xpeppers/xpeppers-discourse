9ade2ce17f8ab1b9a22179eb785c5cbd88551e4e
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2020-08-25 09:47:22 +0200

    Add bash script for configuring ec2 instance

---
+++ packer-provisioning-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install and configure docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker ubuntu
+sudo useradd -G docker discourse
+sudo usermod -aG docker discourse
+
+# Install prerequisities
+sudo apt --yes install git
+
+# Configure and build discourse
+sudo git clone https://github.com/discourse/discourse_docker.git /var/discourse
+sed -i "s/REPLACE_WITH_DB_URL/$DB_URL/g" /home/ubuntu/app.yml
+sudo cp /home/ubuntu/app.yml /var/discourse/containers/
+sudo chown -R discourse:discourse /var/discourse/
+cd /var/discourse && sudo ./launcher rebuild app
